5f3cf526901924a09e47a71ebb4f3e53f9e593ca
PercyGame <67379967+PercyGame@users.noreply.github.com>
2020-12-16 17:49:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 """
-Ceci est le main AntiAlzheimer project
+Ceci les le main AntiAlzheimer project
 """
-
+file = open("user.gks", "w")
+file.close()
 from tkinter import *
-
 
 
 def co():
     user_name = co_txt_entry.get()
 
+    reference_file = open("user.gks", "r")
     # Créer (ou ouvre s'il existe déjà) le fichier de sauvegarde de l'utilisateur
     fichier = open(user_name + ".gks", "a")
     # fenetre avec les exercices etc...
     windowAl = Tk()
-        
+
     # variable pour la boucle du programme
     run = True
-        
+
     """
     Boucle du programme :
     Avantage d'un "while":
@@ -25,7 +26,6 @@ def co():
     - Plus il y a d'instruction, plus la boucle et longue et donc plus il y a de laggs potentiels
     """
     while run:
-        
         label_Al = Label(windowAl, text="Bonjour " + user_name, font=("Courrier", 40), bg='#ffd100', fg='white')
         windowAl.title(user_name)
         windowAl.geometry("1200x750")
@@ -33,16 +33,15 @@ def co():
         windowAl.config(background="#ffd100")
         main_button = Button(windowAl, text="menu", font=("courrier", 25), bg='#ffd100', fg='white')
         main_button.pack()
-        main_button.place(x=10, y=10)
+        main_button.place(x=5, y=5)
         label_Al.pack()
         windowAl.mainloop()
-
 
 
 # créer la fenetre du logeur
 window = Tk()
 
-#personaliser fenetre
+# personaliser fenetre
 window.title("Science plus ( par Henry Legrand )")
 window.geometry("1080x720")
 window.minsize(480, 360)
@@ -61,7 +60,6 @@ co_txt_entry = Entry(frame, text="mot de passe", font=("Courrier", 20), bg='whit
 # bouton
 co_button = Button(frame, text="Connexion", font=("Courrier", 20), bg='white', fg='black', command=co)
 
-    
 # tout afficher
 label_titel.pack()
 label_connexion.pack()
