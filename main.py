93c345faf0c6440b8bff39a2462006a3ae2efbdc
PercyGame <67379967+PercyGame@users.noreply.github.com>
2020-12-17 18:32:52 +0100

    Update main.py
    Ajouté une interface admin

--- main.py
+++ main.py
@@ -5,9 +5,38 @@ file = open("user.gks", "w")
 file.close()
 from tkinter import *
 
+def admin_interface():
+    windowAl = Tk()
+
+    # variable pour la boucle du programme
+    run = True
+
+    """
+    Boucle du programme :
+    Avantage d'un "while":
+    - on peut la casser à tout moment si on a besoin
+    Inconvénients:
+    - Plus il y a d'instruction, plus la boucle et longue et donc plus il y a de laggs potentiels
+    """
+    while run:
+        label_Al = Label(windowAl, text="___/Interface administrateur\___", font=("Courrier", 40), bg='#395D67', fg='#246D39')
+        windowAl.title(user_name)
+        windowAl.geometry("1200x750")
+        windowAl.minsize(480, 360)
+        windowAl.config(background="#395D67")
+        main_button = Button(windowAl, text="menu", font=("courrier", 25), bg='#395D67', fg='#246D39')
+        main_button.pack()
+        main_button.place(x=5, y=5)
+        label_Al.pack()
+        windowAl.mainloop()
+
 
 def co():
     user_name = co_txt_entry.get()
+    admin_name = user_name
+    admin_name.upper()
+    if admin_name == "ADMIN":
+        admin_interface()
 
     reference_file = open("user.gks", "r")
     # Créer (ou ouvre s'il existe déjà) le fichier de sauvegarde de l'utilisateur
